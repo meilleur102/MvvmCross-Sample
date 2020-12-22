9c88dc359d4648847658a2e77ca86b74208b0252
FantasticFiasco <mattias.kindborg@gmail.com>
2020-12-22 17:17:03 +0100

    wip

--- MvxBase.Core/ViewModel/DialogViewModel.cs
+++ MvxBase.Core/ViewModel/DialogViewModel.cs
@@ -1,14 +1,42 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
 using MvvmDialogs;
 
 namespace MvxBase.Core.ViewModel
 {
     public class DialogViewModel : MvxViewModel, IModalDialogViewModel
     {
+        private int _value;
+
         public DialogViewModel()
         {
+            IncreaseCommand = new MvxCommand(Increase);
+            DecreaseCommand = new MvxCommand(Decrease);
+        }
+
+        public IMvxCommand IncreaseCommand { get; }
+        public IMvxCommand DecreaseCommand { get; }
+
+        public int Value
+        {
+            get => _value;
+            set
+            {
+                _value = value;
+                RaisePropertyChanged(() => Value);
+            }
         }
 
         public bool? DialogResult { get; }
+        
+        private void Increase()
+        {
+            Value++;
+        }
+
+        private void Decrease()
+        {
+            Value--;
+        }
     }
 }
