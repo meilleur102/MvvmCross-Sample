150abf84d85ff0b1a6596c71ece0adbc30eff744
FantasticFiasco <mattias.kindborg@gmail.com>
2020-12-22 16:41:27 +0100

    wip

--- MvxBase.Core/ViewModel/RootViewModel.cs
+++ MvxBase.Core/ViewModel/RootViewModel.cs
@@ -12,26 +12,22 @@ namespace MvxBase.Core.ViewModel
         public RootViewModel(IMvxNavigationService navigationService)
         {
             _navigationService = navigationService;
+
+            NavigateCommand = new MvxCommand(Navigate);
             OpenDialogCommand = new MvxCommand(OpenDialog);
         }
 
-        public IMvxCommand OpenDialogCommand { get; set; }
+        public IMvxCommand NavigateCommand { get; }
+        public IMvxCommand OpenDialogCommand { get; }
+
+        private void Navigate()
+        {
+            _navigationService.Navigate<DialogViewModel>();
+        }
 
         private void OpenDialog()
         {
-            //Use MvvmDialog to show DialogViewModel 
-            //If you dont want to use the navigation service 
-            //just remove it from the RootViewModelConstructor (IMvxNavigationService navigationService)
-            //You can pass a object to the new ViewModel if you need to by adding a async to the OpenDialog()
-            //and calling the navigationService like this
-            // await _navigationService.Navigate<DialogViewModel, MyObject>(new MyObject());
-            //and having the Object on a Prepare() Method on the DialogViewModel class.
-            //See link for the MvvmCross Doc
-            //https://www.mvvmcross.com/documentation/fundamentals/navigation
-            //The problem I have is to implement the IoC from Mvvm
-
-
-            _navigationService.Navigate<DialogViewModel>();
+            // TODO: Implement
         }
     }
 }
